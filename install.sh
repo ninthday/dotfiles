9efa340b92b83b85d76e12b49e22101983af129d
Tuvix Shih <tuvix@ninthday.info>
2018-05-17 13:38:28 +0800

    add install shell script

---
+++ install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -e
+
+# Clean up targets
+rm -rf $HOME/.vim* $HOME/.zsh* $HOME/.tmux* $HOME/.fonts
+
+# Symlink files
+ln -s $PWD/zshrc $HOME/.zshrc
+ln -s $PWD/tmux.conf $HOME/.tmux.conf
+ln -s $PWD/vimrc $HOME/.vimrc
