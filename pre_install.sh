854eb0eff2f3b1dbbfbb66648603173d8796ac1c
Tuvix Shih <tuvix@ninthday.info>
2022-03-07 00:18:48 +0800

    新增 poetry 預設安裝

--- pre_install.sh
+++ pre_install.sh
@@ -9,3 +9,6 @@ sudo apt install exfat-fuse exfat-utils
 
 # Checkout pyenv from GitHub
 git clone https://github.com/pyenv/pyenv.git ~/.pyenv
+
+# Install Poetry (Python)
+curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | python3
